5594d0bbfa02ec1c0cd189d0fa24cd59a908a3a7
Jared Knoblauch <jaredknoblauch@gmail.com>
2018-06-13 17:28:47 -0400

    adding actual setup scripts

---
+++ msh_est/__init__.py
@@ -0,0 +1,2 @@
+from .aae_work import run_estimator
+from .msh_from_vcf import getmsh
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+
+setup(name="msh_est",
+      version="0.0.1",
+      description="MSH calculation and estimator code")
