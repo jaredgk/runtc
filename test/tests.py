66153a3f314660b0ec76eb4ec677c60ce82368ed
Jared Knoblauch <jaredknoblauch@gmail.com>
2019-07-26 15:43:53 -0400

    fixing tests

--- test/tests.py
+++ test/tests.py
@@ -9,32 +9,32 @@ from msh_from_vcf import getmsh
 class basicTest(unittest.TestCase):
     
     def test_alpha_withsgtn(self):
-        args = ['--vcf','test_head.vcf','--include-singletons','--out','test_head_ll.txt']
+        args = ['--vcf','test_head.vcf','--out','test_head_ll.txt']
         getmsh(args)
         self.assertTrue(filecmp.cmp('test_head_ll.txt','test_head_alpha_ws.txt'))
     
     def test_alpha_nosgtn(self):
-        args = ['--vcf','test_head.vcf','--out','test_head_ll.txt']
+        args = ['--vcf','test_head.vcf','--out','test_head_ll.txt','--exclude-singletons']
         getmsh(args)
         self.assertTrue(filecmp.cmp('test_head_ll.txt','test_head_alpha_ns.txt'))
 
     def test_singleton_withsgtn(self):
-        args = ['--vcf','test_head.vcf','--include-singletons','--singleton','--out','test_head_ll.txt']
+        args = ['--vcf','test_head.vcf','--singleton','--out','test_head_ll.txt']
         getmsh(args)
         self.assertTrue(filecmp.cmp('test_head_ll.txt','test_head_sing_ws.txt'))
 
     def test_singleton_nosgtn(self):
-        args = ['--vcf','test_head.vcf','--singleton','--out','test_head_ll.txt']
+        args = ['--vcf','test_head.vcf','--singleton','--out','test_head_ll.txt','--exclude-singletons']
         getmsh(args)
         self.assertTrue(filecmp.cmp('test_head_ll.txt','test_head_sing_ns.txt'))
 
     def test_kall_withsgtn(self):
-        args = ['--vcf','test_head.vcf','--k-all','--out','test_head_ll.txt','--include-singletons','--singleton']
+        args = ['--vcf','test_head.vcf','--k-all','--out','test_head_ll.txt']
         getmsh(args)
         self.assertTrue(filecmp.cmp('test_head_ll.txt','test_head_kall_ws.txt'))
 
     def test_kall_nosgtn(self):
-        args = ['--vcf','test_head.vcf','--k-all','--out','test_head_ll.txt','--singleton']
+        args = ['--vcf','test_head.vcf','--k-all','--out','test_head_ll.txt','--exclude-singletons']
         getmsh(args)
         self.assertTrue(filecmp.cmp('test_head_ll.txt','test_head_kall_ns.txt'))
 
